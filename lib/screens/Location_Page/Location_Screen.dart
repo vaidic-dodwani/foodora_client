aa3672a58b2c4e2b67eec789a8520448aaeccc79
vaidic-dodwani <vaidicdodwani@gmail.com>
2022-10-25 14:30:46 +0530

    Added Location Permission and its display

--- lib/screens/Location_Page/Location_Screen.dart
+++ lib/screens/Location_Page/Location_Screen.dart
@@ -1,13 +1,25 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:foodora/designing.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:location/location.dart';
 
-class location_screen extends StatelessWidget {
+class location_screen extends StatefulWidget {
   const location_screen({super.key});
 
   @override
+  State<location_screen> createState() => _location_screenState();
+}
+
+class _location_screenState extends State<location_screen> {
+  bool _location_recieved = false;
+  LocationData? current_location;
+  @override
   Widget build(BuildContext context) {
+    final location = Location();
+
     final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
@@ -46,6 +58,7 @@ class location_screen extends StatelessWidget {
               //
               SizedBox(height: size.height * 0.05),
               //
+
               button_style(
                 'Enter Manually',
                 context,
@@ -55,12 +68,43 @@ class location_screen extends StatelessWidget {
               SizedBox(
                 height: size.height * 0.025,
               ),
-              button_style('Current Location', context,
-                  color: orange_button_color)
+
+              _location_recieved
+                  ? lat_long_display(current_location)
+                  : button_style(
+                      'Current Location',
+                      context,
+                      color: orange_button_color,
+                      function: () async {
+                        if (await Permission.location.request().isGranted) {
+                          current_location = await location.getLocation();
+                          log(current_location.toString());
+                          _location_recieved = true;
+                          setState(() {});
+                        }
+                      },
+                    )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget lat_long_display(LocationData? current_location) {
+  return (current_location == null
+      ? Text("null")
+      : Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Lattitude=" + current_location.latitude.toString(),
+            ),
+            SizedBox(width: 10),
+            Text(
+              "Longitude " + current_location.longitude.toString(),
+            ),
+          ],
+        ));
 }
